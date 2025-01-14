146ded262b4528a789131c07b771092c49d97320
DanielleIsMe <danielle.oconnellp@gmail.com>
2025-01-14 14:15:46 +1100

    guessing game final

--- live/scripts/guessingGame.js
+++ live/scripts/guessingGame.js
@@ -1,11 +1,12 @@
 let randomNumber = Math.floor(Math.random() * 100) + 1;
 
 function guess() {
-    let inputElement = parseFloat(document.getElementById("input").value);
+    let inputElement = parseInt(document.getElementById("input").value);
     let feedbackElement = document.getElementById("feedback");
 
     if (inputElement === randomNumber) {
         feedbackElement.innerHTML = "Congratulations! You guessed correctly."
+        feedbackElement.style.color = "green"
     } else if (inputElement < randomNumber) {
         feedbackElement.innerHTML = "Too low, sorry! Try again."
     } else {
